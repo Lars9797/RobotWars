43369dd0074d973e45e0c53553e7137cd982a585
Lars9797 <Lars-Oliver.Peters@btc-ag.com>
2024-09-19 08:57:26 +0200

    Add files via upload

---
+++ RobotWars.java
@@ -0,0 +1,13 @@
+package com.btcag.bootcamp;
+
+public class RobotWars {
+    public static void main(String[] args) {
+        x = 0, y=0
+                while(y<15)
+                    while(x<15)
+                        print([])
+                        print
+                                x++
+                                        y++
+    }
+}
